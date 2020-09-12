16aa42fdf1a477abf3b58fe62d953229d2474b8e
MJ Johnson <mjohnson4685@gmail.com>
2020-09-12 21:43:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,7 @@
 
 def oxford_comma(array)
 
-  if array.length == 1
-return "kiwi"
-  elsif array.length == 2
-array.join("and")
-  elsif array.length == 3
-array.join(",",",","and")
- elsif array
-
+  if array.length < 3
+    array.join("and")
   end
 end
